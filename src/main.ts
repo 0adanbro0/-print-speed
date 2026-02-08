c95f56a6f32af431de970cd9ac7494bf66fd1d80
Matvey <adanbro2009@gmail.com>
2026-02-08 20:42:50 +0300

    feat : end with enter Char system and start build e-keyboard

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import string_default_text from "./data/database.ts";
 import random from "./getRandom/getRandomInt.ts";
 
 let array_default_text:string[]
-let array_introduced_txt:string[]
+let array_introduced_txt:string[] = [];
+
+let game_field = document.querySelector('.game-main_text');
+let user_text = document.querySelector('.game-user_text');
 
 let language_index:number = 0;
 
@@ -11,7 +14,16 @@ main();
 function main(){
     ConvertToArray(language_index)
 
-    ShowDefaultText();
+    ShowText()
+}
+
+function ShowText(){
+    let game_field = document.querySelector('.game-main_text');
+    if(!game_field) return;
+
+    for(let i = 0; i < array_default_text.length; i++){
+        game_field.textContent += array_default_text[i]
+    }
 }
 
 function ConvertToArray(language:number){
@@ -20,18 +32,38 @@ function ConvertToArray(language:number){
     string_default_text[language][ random(0, string_default_text[language].length - 1)].split("");
 }
 
-function ShowDefaultText(){
-    let game_field = document.querySelector('.game-main_text');
+function Game(arg:boolean){
+    if(!user_text) return;
     if(!game_field) return;
 
-    game_field.innerHTML = "";
+    if(arg){
+        user_text.textContent = "";
+        array_introduced_txt.push(array_default_text[0])
+        for(let i = 0; i < array_introduced_txt.length; i++){
+            array_introduced_txt[i] == " " ? user_text.textContent+= '\u00A0' : user_text.textContent += array_introduced_txt[i]
+        }
 
-    for(let i = 0; i < array_default_text.length; i++){
-        array_default_text[i] == " " ? game_field.innerHTML+= "&nbsp" : game_field.innerHTML += array_default_text[i]
+        array_default_text.splice(0, 1);
+
+        game_field.textContent = "";
+
+        for(let i = 0; i < array_default_text.length; i++){
+            array_default_text[i] == " " ? game_field.textContent+= '\u00A0' : game_field.textContent += array_default_text[i]
+        }
+    }
+    else if(!arg){
+        const error = document.createElement('span')
+        error.style.backgroundColor = "red";
+        error.textContent = array_default_text[0];
+
+        if(user_text.textContent.at(-1) !== error.textContent){
+            user_text.appendChild(error);
+        }
     }
 }
 
 document.addEventListener('keydown', (PressedKey)=>{
+    if (PressedKey.key.length > 1) return;
     let input_keyboard = document.querySelector('#input_keyboard') as HTMLInputElement;
     if(!input_keyboard){
         return;
@@ -39,14 +71,11 @@ document.addEventListener('keydown', (PressedKey)=>{
     input_keyboard.value = "";
 
     console.log(PressedKey.key)
+
+    if(PressedKey.key == array_default_text[0]) Game(true);
+    else if(PressedKey.key !== array_default_text[0]) Game(false);
+
     
-    if(PressedKey.key == array_default_text[0]){
-        array_default_text.splice(0, 1);
-        console.log(array_default_text)
-    }
-
-    ShowDefaultText();
-
 });
 
 
