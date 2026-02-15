870d375a62b5b07f777ba0b034096255898b8f6f
Matvey <adanbro2009@gmail.com>
2026-02-15 23:28:34 +0300

    feat: add check for outcoming | correct | incorrect symbols

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,28 +1,35 @@
 import EKeyboard from "./EKeyboard"
 import Input from "./Input"
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import string_default_text from '../data/database'
 
-let defaultArray:string[] = string_default_text[0][0].split("");
-console.log(defaultArray)
-
 const App = () => {
-    let [content, SetContent] = useState<string>(defaultArray.join(''));
+    const [arrayCharNodes, SetArrayCharNodes] = useState<{char:string, status:string}[]>(string_default_text[0][0].split("").map(symbol=>({
+                                                                                            char: symbol,
+                                                                                            status: 'outcoming'
+                                                                                        })));
+    const [currentIndex, SetCurrentIndex] = useState<number>(0);
     useEffect(() => {
         const handleKeyDown = ((e: KeyboardEvent)=>{
             if(e.key.length > 1) return;
+            if (currentIndex >= arrayCharNodes.length) return;
+            console.log(e.key);
 
-            console.log(e.key)
+            const newArrayClassName:{char:string, status:string}[] = [...arrayCharNodes];
 
-                SetContent((PrevContent) => {
-                    if (e.key === PrevContent[0]) {
-                        const newArray = PrevContent.slice(1)
-                        return newArray;
-                    }
-                    return PrevContent;
-                });
+            if (e.key === arrayCharNodes[currentIndex].char) {
+                newArrayClassName[currentIndex].status = 'correct';
+                
+                SetCurrentIndex(prev => prev + 1);
+                console.log(currentIndex)
+            }
+            else{
+                newArrayClassName[currentIndex].status = 'incorrect';
+            }
+            SetArrayCharNodes(newArrayClassName)
+            console.log(arrayCharNodes);
         })
 
         window.addEventListener('keydown', handleKeyDown)
@@ -30,13 +37,17 @@ const App = () => {
         return () => {
             window.removeEventListener('keydown', handleKeyDown);
         }
-    }, []);
+    }, [currentIndex, arrayCharNodes]);
 
 
     const eKeys:string[] = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a' , 's' , 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', 'Space'];
     return (
         <>
-        <pre>{content}</pre>
+        <div className="interactive_text_place">
+            {arrayCharNodes.map((element, index) => (
+                <span key={index} className={`char ${element.status}`}>{element.char}</span>
+            ))}
+        </div>
         <Input Content={' '} />
         <div className="keyboard_place">
             {eKeys.map((element, index) => (
